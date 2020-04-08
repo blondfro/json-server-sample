27d5f5086075cd44100632bf5ffdb0f129d25901
James Morris <blondfro@gmail.com>
2020-04-08 17:05:02 -0500

    created custom routes for users.

--- mockBackend/tools/server.js
+++ mockBackend/tools/server.js
@@ -15,8 +15,42 @@ server.use(jsonServer.bodyParser);
 server.use(jsonServer.rewriter({ "/api/users": "/users" }));
 
 server.get("/users", (req, res) => {
-  let results = db.users;
-  res.json(results);
+  res.json(db.users);
+});
+
+server.get("/users/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let user = db.users.find((user) => user.id === id);
+  res.json(user);
+});
+
+server.post("/users", (req, res) => {
+  let id = db.users.length + 1;
+  let newUser = { id, ...req.body };
+  let newDb = [newUser, ...db.users].sort((a, b) => a.id - b.id);
+  db.users = [...newDb];
+  res.json(db.users);
+});
+
+server.put("/users/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let newUser = req.body;
+  if (req.body.id) {
+    newUser = req.body;
+  } else newUser = { id, ...req.body };
+
+  let newDb = [newUser, ...db.users.filter((user) => user.id !== id)].sort(
+    (a, b) => a.id - b.id
+  );
+  db.users = [...newDb];
+  res.json(db.users);
+});
+
+server.delete("/users/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let newDb = [...db.users.filter((user) => user.id !== id)];
+  db.users = [...newDb];
+  res.json(db);
 });
 
 server.use(router);
