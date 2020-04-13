74fd7d3da6051b423a014591146e2a5f943da3d0
James Morris <blondfro@gmail.com>
2020-04-13 16:12:05 -0500

    added in some basic styles for the navigation.

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -2,18 +2,36 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Navigation(props) {
-  const activeStyle = { color: "blue" };
+  const activeStyle = {
+    backgroundColor: "#2168A3",
+  };
+
+  const inActiveStyle = {
+    font: "22px Arial",
+    textDecoration: "none",
+    backgroundColor: "#2791B4",
+    color: "white",
+    padding: "2px 6px 2px 6px",
+    width: "100px",
+    height: "80px",
+    borderTop: "1px solid #CCCCCC",
+    borderRight: "1px solid #333333",
+    borderBottom: "1px solid #333333",
+    borderLeft: "1px solid #CCCCCC",
+    borderRadius: "5px",
+  };
+
   return (
     <nav>
-      <NavLink to="/" activeStyle={activeStyle} exact>
+      <NavLink to="/" style={inActiveStyle} activeStyle={activeStyle} exact>
         Home
       </NavLink>
       {"|"}
-      <NavLink to="/users" activeStyle={activeStyle}>
+      <NavLink to="/users" style={inActiveStyle} activeStyle={activeStyle}>
         Users
       </NavLink>
       {"|"}
-      <NavLink to="/about" activeStyle={activeStyle}>
+      <NavLink to="/about" style={inActiveStyle} activeStyle={activeStyle}>
         About
       </NavLink>
       {"|"}
